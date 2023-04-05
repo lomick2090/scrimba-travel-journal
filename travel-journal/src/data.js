a9f5c406fc3dfbd6719d87675a04b5de73d957ee
minkman <lomick2090@gmail.com>
2023-04-05 09:37:18 -0500

    Initialize files

---
+++ travel-journal/src/data.js
@@ -0,0 +1,47 @@
+export default [
+
+    {
+        place:'Thailand',
+        startDate:'March 01, 2023',
+        endDate:'March 30, 2023',
+        description:`Located in the heart of mainland Southeast Asia, Thailand is a country of mountains, 
+        hills, plains and a long coastline along the Gulf of Thailand (1,875 km) and the Andaman Sea 
+        (740 km), not including the coastlines of some 400 islands, most of them in the Andaman Sea.`,
+        google:'https://www.google.com/maps/place/Thailand/',
+        pic:'thailand.jpg'
+    },
+    {
+        place:'Lison Portugal',
+        startDate:'November 01, 2022',
+        endDate:'November 08, 2022',
+        description:`Lisbon is the capital and the largest city of Portugal. With over 20 centuries of History, 
+        this city located on the 7 hills has more than 500,000 inhabitants. Lisbon shows multiple contrasts 
+        between the historical and modern, combining different cultures, trends and lifestyles.`,
+        google:'https://www.google.com/maps/place/Lisbon,+Portugal',
+        pic:'lisbon.jpg'
+    },
+    {
+        place:'Paris, Fance',
+        startDate:'November 14, 2022',
+        endDate:'Novemember 19, 2022',
+        description:`Paris is one of the most beautiful cities in the world. It is known worldwide for the Louvre
+         Museum, Notre-Dame cathedral, and the Eiffel tower. It has a reputation of being a romantic and cultural 
+         city. The city is also known for its high-quality gastronomy and the terraces of its cafés.`,
+        google:'https://www.google.com/maps/place/Paris,+France',
+        pic:'paris.jpg'
+    },
+    {
+        place:'Amsterdam, Netherlands',
+        startDate:'November 21, 2022',
+        endDate:'November 23, 2022',
+        description:`It is the capital and the principal commercial and financial centre of the Netherlands. To the
+         scores of tourists who visit each year, Amsterdam is known for its historical attractions, for its collections
+          of great art, and for the distinctive colour and flavour of its old sections, which have been so well preserved.`,
+        google:'https://www.google.com/maps/place/Amsterdam,+Netherlands',
+        pic:'amsterdam.jpg'
+    },
+
+
+
+
+]
